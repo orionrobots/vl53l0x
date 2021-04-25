f7053061bb733c7fcbf7621c50d7b334d18c65c6
Danny Staple <danny@orionrobots.co.uk>
2021-04-25 14:13:32 +0100

    Use consts to save memory.

--- vl53l0x.py
+++ vl53l0x.py
@@ -22,94 +22,94 @@ _RESULT_RANGE_STATUS = const(0x14)
 _OSC_CALIBRATE = const(0xf8)
 _MEASURE_PERIOD = const(0x04)
 
-SYSRANGE_START = 0x00
+SYSRANGE_START = const(0x00)
 
-SYSTEM_THRESH_HIGH = 0x0C
-SYSTEM_THRESH_LOW = 0x0E
+SYSTEM_THRESH_HIGH = const(0x0C)
+SYSTEM_THRESH_LOW = const(0x0E)
 
-SYSTEM_SEQUENCE_CONFIG = 0x01
-SYSTEM_RANGE_CONFIG = 0x09
-SYSTEM_INTERMEASUREMENT_PERIOD = 0x04
+SYSTEM_SEQUENCE_CONFIG = const(0x01)
+SYSTEM_RANGE_CONFIG = const(0x09)
+SYSTEM_INTERMEASUREMENT_PERIOD = const(0x04)
 
-SYSTEM_INTERRUPT_CONFIG_GPIO = 0x0A
+SYSTEM_INTERRUPT_CONFIG_GPIO = const(0x0A)
 
-GPIO_HV_MUX_ACTIVE_HIGH = 0x84
+GPIO_HV_MUX_ACTIVE_HIGH = const(0x84)
 
-SYSTEM_INTERRUPT_CLEAR = 0x0B
+SYSTEM_INTERRUPT_CLEAR = const(0x0B)
 
-RESULT_INTERRUPT_STATUS = 0x13
-RESULT_RANGE_STATUS = 0x14
+RESULT_INTERRUPT_STATUS = const(0x13)
+RESULT_RANGE_STATUS = const(0x14)
 
-RESULT_CORE_AMBIENT_WINDOW_EVENTS_RTN = 0xBC
-RESULT_CORE_RANGING_TOTAL_EVENTS_RTN = 0xC0
-RESULT_CORE_AMBIENT_WINDOW_EVENTS_REF = 0xD0
-RESULT_CORE_RANGING_TOTAL_EVENTS_REF = 0xD4
-RESULT_PEAK_SIGNAL_RATE_REF = 0xB6
+RESULT_CORE_AMBIENT_WINDOW_EVENTS_RTN = const(0xBC)
+RESULT_CORE_RANGING_TOTAL_EVENTS_RTN = const(0xC0)
+RESULT_CORE_AMBIENT_WINDOW_EVENTS_REF = const(0xD0)
+RESULT_CORE_RANGING_TOTAL_EVENTS_REF = const(0xD4)
+RESULT_PEAK_SIGNAL_RATE_REF = const(0xB6)
 
-ALGO_PART_TO_PART_RANGE_OFFSET_MM = 0x28
+ALGO_PART_TO_PART_RANGE_OFFSET_MM = const(0x28)
 
-I2C_SLAVE_DEVICE_ADDRESS = 0x8A
+I2C_SLAVE_DEVICE_ADDRESS = const(0x8A)
 
-MSRC_CONFIG_CONTROL = 0x60
+MSRC_CONFIG_CONTROL = const(0x60)
 
-PRE_RANGE_CONFIG_MIN_SNR = 0x27
-PRE_RANGE_CONFIG_VALID_PHASE_LOW = 0x56
-PRE_RANGE_CONFIG_VALID_PHASE_HIGH = 0x57
-PRE_RANGE_MIN_COUNT_RATE_RTN_LIMIT = 0x64
+PRE_RANGE_CONFIG_MIN_SNR = const(0x27)
+PRE_RANGE_CONFIG_VALID_PHASE_LOW = const(0x56)
+PRE_RANGE_CONFIG_VALID_PHASE_HIGH = const(0x57)
+PRE_RANGE_MIN_COUNT_RATE_RTN_LIMIT = const(0x64)
 
-FINAL_RANGE_CONFIG_MIN_SNR = 0x67
-FINAL_RANGE_CONFIG_VALID_PHASE_LOW = 0x47
-FINAL_RANGE_CONFIG_VALID_PHASE_HIGH = 0x48
-FINAL_RANGE_CONFIG_MIN_COUNT_RATE_RTN_LIMIT = 0x44
+FINAL_RANGE_CONFIG_MIN_SNR = const(0x67)
+FINAL_RANGE_CONFIG_VALID_PHASE_LOW = const(0x47)
+FINAL_RANGE_CONFIG_VALID_PHASE_HIGH = const(0x48)
+FINAL_RANGE_CONFIG_MIN_COUNT_RATE_RTN_LIMIT = const(0x44)
 
-PRE_RANGE_CONFIG_SIGMA_THRESH_HI = 0x61
-PRE_RANGE_CONFIG_SIGMA_THRESH_LO = 0x62
+PRE_RANGE_CONFIG_SIGMA_THRESH_HI = const(0x61)
+PRE_RANGE_CONFIG_SIGMA_THRESH_LO = const(0x62)
 
-PRE_RANGE_CONFIG_VCSEL_PERIOD = 0x50
-PRE_RANGE_CONFIG_TIMEOUT_MACROP_HI = 0x51
-PRE_RANGE_CONFIG_TIMEOUT_MACROP_LO = 0x52
+PRE_RANGE_CONFIG_VCSEL_PERIOD = const(0x50)
+PRE_RANGE_CONFIG_TIMEOUT_MACROP_HI = const(0x51)
+PRE_RANGE_CONFIG_TIMEOUT_MACROP_LO = const(0x52)
 
-SYSTEM_HISTOGRAM_BIN = 0x81
-HISTOGRAM_CONFIG_INITIAL_PHASE_SELECT = 0x33
-HISTOGRAM_CONFIG_READOUT_CTRL = 0x55
+SYSTEM_HISTOGRAM_BIN = const(0x81)
+HISTOGRAM_CONFIG_INITIAL_PHASE_SELECT = const(0x33)
+HISTOGRAM_CONFIG_READOUT_CTRL = const(0x55)
 
-FINAL_RANGE_CONFIG_VCSEL_PERIOD = 0x70
-FINAL_RANGE_CONFIG_TIMEOUT_MACROP_HI = 0x71
-FINAL_RANGE_CONFIG_TIMEOUT_MACROP_LO = 0x72
-CROSSTALK_COMPENSATION_PEAK_RATE_MCPS = 0x20
+FINAL_RANGE_CONFIG_VCSEL_PERIOD = const(0x70)
+FINAL_RANGE_CONFIG_TIMEOUT_MACROP_HI = const(0x71)
+FINAL_RANGE_CONFIG_TIMEOUT_MACROP_LO = const(0x72)
+CROSSTALK_COMPENSATION_PEAK_RATE_MCPS = const(0x20)
 
-MSRC_CONFIG_TIMEOUT_MACROP = 0x46
+MSRC_CONFIG_TIMEOUT_MACROP = const(0x46)
 
-SOFT_RESET_GO2_SOFT_RESET_N = 0xBF
-IDENTIFICATION_MODEL_ID = 0xC0
-IDENTIFICATION_REVISION_ID = 0xC2
+SOFT_RESET_GO2_SOFT_RESET_N = const(0xBF)
+IDENTIFICATION_MODEL_ID = const(0xC0)
+IDENTIFICATION_REVISION_ID = const(0xC2)
 
-OSC_CALIBRATE_VAL = 0xF8
+OSC_CALIBRATE_VAL = const(0xF8)
 
-GLOBAL_CONFIG_VCSEL_WIDTH = 0x32
-GLOBAL_CONFIG_SPAD_ENABLES_REF_0 = 0xB0
-GLOBAL_CONFIG_SPAD_ENABLES_REF_1 = 0xB1
-GLOBAL_CONFIG_SPAD_ENABLES_REF_2 = 0xB2
-GLOBAL_CONFIG_SPAD_ENABLES_REF_3 = 0xB3
-GLOBAL_CONFIG_SPAD_ENABLES_REF_4 = 0xB4
-GLOBAL_CONFIG_SPAD_ENABLES_REF_5 = 0xB5
+GLOBAL_CONFIG_VCSEL_WIDTH = const(0x32)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_0 = const(0xB0)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_1 = const(0xB1)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_2 = const(0xB2)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_3 = const(0xB3)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_4 = const(0xB4)
+GLOBAL_CONFIG_SPAD_ENABLES_REF_5 = const(0xB5)
 
-GLOBAL_CONFIG_REF_EN_START_SELECT = 0xB6
-DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD = 0x4E
-DYNAMIC_SPAD_REF_EN_START_OFFSET = 0x4F
-POWER_MANAGEMENT_GO1_POWER_FORCE = 0x80
+GLOBAL_CONFIG_REF_EN_START_SELECT = const(0xB6)
+DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD = const(0x4E)
+DYNAMIC_SPAD_REF_EN_START_OFFSET = const(0x4F)
+POWER_MANAGEMENT_GO1_POWER_FORCE = const(0x80)
 
-VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV = 0x89
+VHV_CONFIG_PAD_SCL_SDA__EXTSUP_HV = const(0x89)
 
-ALGO_PHASECAL_LIM = 0x30
-ALGO_PHASECAL_CONFIG_TIMEOUT = 0x30
+ALGO_PHASECAL_LIM = const(0x30)
+ALGO_PHASECAL_CONFIG_TIMEOUT = const(0x30)
 
 
 class TimeoutError(RuntimeError):
     pass
 
 
-class VL53L0X():
+class VL53L0X:
     def __init__(self, i2c, address=0x29):
         self.i2c = i2c
         self.address = address
@@ -451,10 +451,10 @@ class VL53L0X():
         return True
 
     def decode_Vcsel_period(self, reg_val):
-        return (((reg_val) + 1) << 1)
+        return ((reg_val) + 1) << 1
 
     def encode_Vcsel_period(self, period_pclks):
-        return (((period_pclks) >> 1) - 1)
+        return ((period_pclks) >> 1) - 1
 
     def set_Vcsel_pulse_period(self, type, period_pclks):
         vcsel_period_reg = self.encode_Vcsel_period(period_pclks)
@@ -486,37 +486,45 @@ class VL53L0X():
             self._register(MSRC_CONFIG_TIMEOUT_MACROP, 255 if new_msrc_timeout_mclks > 256 else (new_msrc_timeout_mclks - 1))
         elif type == self.vcsel_period_type[1]:
             if period_pclks == 8:
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x10)
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08)
-                self._register(GLOBAL_CONFIG_VCSEL_WIDTH, 0x02)
-                self._register(ALGO_PHASECAL_CONFIG_TIMEOUT, 0x0C)
-                self._register(0xFF, 0x01)
-                self._register(ALGO_PHASECAL_LIM, 0x30)
-                self._register(0xFF, 0x00)
+                self._config(
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x10),
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08),
+                    (GLOBAL_CONFIG_VCSEL_WIDTH, 0x02),
+                    (ALGO_PHASECAL_CONFIG_TIMEOUT, 0x0C),
+                    (0xFF, 0x01),
+                    (ALGO_PHASECAL_LIM, 0x30),
+                    (0xFF, 0x00)
+                )
             elif period_pclks == 10:
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x28)
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08)
-                self._register(GLOBAL_CONFIG_VCSEL_WIDTH, 0x03)
-                self._register(ALGO_PHASECAL_CONFIG_TIMEOUT, 0x09)
-                self._register(0xFF, 0x01)
-                self._register(ALGO_PHASECAL_LIM, 0x20)
-                self._register(0xFF, 0x00)
+                self._config(
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x28),
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08),
+                    (GLOBAL_CONFIG_VCSEL_WIDTH, 0x03),
+                    (ALGO_PHASECAL_CONFIG_TIMEOUT, 0x09),
+                    (0xFF, 0x01),
+                    (ALGO_PHASECAL_LIM, 0x20),
+                    (0xFF, 0x00)
+                )
             elif period_pclks == 12:
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x38)
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08)
-                self._register(GLOBAL_CONFIG_VCSEL_WIDTH, 0x03)
-                self._register(ALGO_PHASECAL_CONFIG_TIMEOUT, 0x08)
-                self._register(0xFF, 0x01)
-                self._register(ALGO_PHASECAL_LIM, 0x20)
-                self._register(0xFF, 0x00)
+                self._config(
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x38),
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08),
+                    (GLOBAL_CONFIG_VCSEL_WIDTH, 0x03),
+                    (ALGO_PHASECAL_CONFIG_TIMEOUT, 0x08),
+                    (0xFF, 0x01),
+                    (ALGO_PHASECAL_LIM, 0x20),
+                    (0xFF, 0x00)
+                )
             elif period_pclks == 14:
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x48)
-                self._register(FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08)
-                self._register(GLOBAL_CONFIG_VCSEL_WIDTH, 0x03)
-                self._register(ALGO_PHASECAL_CONFIG_TIMEOUT, 0x07)
-                self._register(0xFF, 0x01)
-                self._register(ALGO_PHASECAL_LIM, 0x20)
-                self._register(0xFF, 0x00)
+                self._config(
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_HIGH, 0x48),
+                    (FINAL_RANGE_CONFIG_VALID_PHASE_LOW, 0x08),
+                    (GLOBAL_CONFIG_VCSEL_WIDTH, 0x03),
+                    (ALGO_PHASECAL_CONFIG_TIMEOUT, 0x07),
+                    (0xFF, 0x01),
+                    (ALGO_PHASECAL_LIM, 0x20),
+                    (0xFF, 0x00)
+                )
             else:
                 return False
 
@@ -579,10 +587,10 @@ class VL53L0X():
 
     def timeout_microseconds_to_Mclks(self, timeout_period_us, vcsel_period_pclks):
         macro_period_ns = self.calc_macro_period(vcsel_period_pclks)
-        return (((timeout_period_us * 1000) + (macro_period_ns / 2)) / macro_period_ns)
+        return ((timeout_period_us * 1000) + (macro_period_ns / 2)) / macro_period_ns
 
     def calc_macro_period(self, vcsel_period_pclks):
-        return (((2304 * (vcsel_period_pclks) * 1655) + 500) / 1000)
+        return ((2304 * (vcsel_period_pclks) * 1655) + 500) / 1000
 
     def decode_timeout(self, reg_val):
         return ((reg_val & 0x00FF) << ((reg_val & 0xFF00) >> 8)) + 1
